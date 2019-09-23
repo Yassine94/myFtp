14e8939b1f5258cfcbe70c2f7f2ce55b9441269b
Sloknatos <adam.louis28@gmail.com>
2019-09-23 09:46:53 +0200

    change on promisify

--- index.js
+++ index.js
@@ -6,35 +6,35 @@ const FTPSession = require('./lib/session')
 
 function ftpd (opts, sessionCb) {
     if (typeof opts === 'string') {
-        const [ host, port ] = opts.split(':')
-        opts = { host: host, port: parseInt(port, 10) }
+        const [ host, port ] = opts.split(':');
+        opts = { host: host, port: parseInt(port, 10) };
     }
 
     opts = Object.assign({
-        host: '127.0.0.1',
-        port: 1337,
+        host: '0.0.0.0',
+        port: 21,
         root: '/',
         maxConnections: 10,
         readOnly: true
-    }, opts)
+    }, opts);
 
-    const ftpServer = createServer()
-    ftpServer.maxConnections = opts.maxConnections
+    const ftpServer = createServer();
+    ftpServer.maxConnections = opts.maxConnections;
 
     if (sessionCb) {
         ftpServer.on('connection', (socket) => {
             const session = new FTPSession(socket, {
                 root: opts.root, host: opts.host, readOnly: opts.readOnly
-            })
+            });
             session.accept(opts.greet).then(() => {
-                sessionCb(session)
+                sessionCb(session);
             })
         })
 
-        ftpServer.listen(opts.port, opts.host)
+        ftpServer.listen(opts.port, opts.host);
     }
 
-    return ftpServer
+    return ftpServer;
 }
 
-module.exports = ftpd
+module.exports = ftpd;
